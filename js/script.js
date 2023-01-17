f34091879488179ad607ddb42ac0d02f3bc35898
Olena <33331@ukr.net>
2023-01-17 10:31:32 +0100

    remove and add 'active'

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
+  const clickedElement = this;
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+
+  }
   /* add class 'active' to the clicked link */
+  clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  console.log(activeArticles);
+
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
 
